7cfbcbd4bc36784a2fcee8207cfb1e2d5babd58f
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-01-01 11:42:16 +0900

    新規登録画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'signup', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
+  get 'confirmation_sent', to: 'users#confirmation_sent', as: 'confirmation_sent'
   # Defines the root path route ("/")
   # root "articles#index"
 end
